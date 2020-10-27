27d67148bf4123fd109dafcc51f608a26140d980
Eddie Crochet <eddiecrochet1994@gmail.com>
2020-10-26 23:34:11 -0500

    Getting caught up with another coding challenge

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -73,3 +73,28 @@ console.log(totals);
 ////////////////////
 // CODING CHALLENGE 3 - OBJECTS
 
+const mark = {
+    fullName : 'Mark Miller',
+    mass: 78,
+    height: 1.69,
+    calcBMI: function () {
+        this.bmi = this.mass / this.height ** 2;
+        return this.bmi;
+    }
+};
+
+const john = {
+    fullName: 'John Smith',
+    mass: 92,
+    height: 1.95,
+    calcBMI: function () {
+        this.bmi = this.mass / this.height ** 2;
+        return this.bmi;
+    }
+};
+
+if (john.calcBMI() > mark.calcBMI()) {
+    console.log(`${john.fullName}'s BMI (${john.calcBMI()}) is greater than ${mark.fullName}'s BMI (${mark.calcBMI()})`);
+} else {
+    console.log(`${mark.fullName}'s BMI (${mark.calcBMI()}) is greater than ${john.fullName}'s BMI (${john.calcBMI()})`);
+}
