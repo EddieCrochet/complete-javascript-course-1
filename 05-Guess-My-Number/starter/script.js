eb120b0b9ae331ae3a32b138b3105d4fa5288b9f
Eddie Crochet <eddiecrochet1994@gmail.com>
2020-11-06 13:45:51 -0600

    My friend came over and saw the app and was all I like I bet u cant change it fropm guessing numbers to letters and I was all like watch me bro

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,18 +1,26 @@
 'use strict';
 
-const number = Math.trunc(Math.random()* 20) + 1;
+const number = Math.trunc(Math.random()* 26) + 1;
+let secretLetter;
 let score = 20;
 let message = document.querySelector('.message');
 let DOMScore = document.querySelector('.score');
 
+let alph = ['A', 'B', 'C', 'D', 'E' ,'F' , 'G', 'H', 'I', 'J' ,'K' , 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S' ,'T' , 'U', 'V', 'W', 'X', 'Y', 'Z'];
+for(let i = 0; i < 26; i++) {
+    secretLetter = alph[number];
+}
+console.log(secretLetter);
+
 document.querySelector('.check').addEventListener
 ('click', function() {
-    const guess = document.querySelector('.guess').value;
+    const guess = document.querySelector('.guess').value.toUpperCase();
+    const isGuessIndex = (el) => el === guess;
+    let guessIndex = alph.findIndex(isGuessIndex);
     
-
     if (!guess) {
-        message.textContent = 'No Number!!!'
-    } else if (guess > number) {
+        message.textContent = 'No Letter!!!'
+    } else if (guessIndex > number) {
         if (score > 1) {
             message.textContent = "Guess was too high!";
             score--;
@@ -21,7 +29,7 @@ document.querySelector('.check').addEventListener
             message.textContent = "You lost the game, nerd!";
             DOMScore.textContent = 0;
         }
-    } else if (guess < number) {
+    } else if (guessIndex < number) {
         if (score > 1) {
             message.textContent = "Guess was too low!";
             score--;
@@ -30,8 +38,8 @@ document.querySelector('.check').addEventListener
             message.textContent = "You lost the game, nerd!";
             DOMScore.textContent = 0;
         }
-    } else if (guess == number) {
-        document.querySelector('.number').textContent = number;
+    } else if (guess == secretLetter) {
+        document.querySelector('.number').textContent = secretLetter;
         document.querySelector('body').style.backgroundColor = '#60b347';
         message.textContent = "Guess was just right, Baby!";
         document.querySelector('.number').style.width = '30rem';
@@ -40,8 +48,13 @@ document.querySelector('.check').addEventListener
 
 document.querySelector('.again').addEventListener
 ('click', function() {
-    const number = Math.trunc(Math.random()* 20) + 1;
+    const number = Math.trunc(Math.random()* 26) + 1;
     let score = 20;
+    let secretLetter;
+    for(let i = 0; i < 26; i++) {
+        secretLetter = alph[number];
+    }
+    console.log(secretLetter);
     message.textContent = "Start guessing...";
     document.querySelector('.number').textContent = "?";
     DOMScore.textContent = score;
