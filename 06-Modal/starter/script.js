fb554e3660bdfed5a596220bc24c2f78cda7e88c
Eddie Crochet <eddiecrochet1994@gmail.com>
2020-12-22 17:40:18 -0600

    playing w css classes

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btn = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+    //attaches handler to all the buttons thru the loop
+    btnsOpenModal[i].addEventListener
+        ('click', function() {
+            console.log('GUTTON LCIKIED>>>');
+            modal.classList.remove('hidden');
+        });
+}
