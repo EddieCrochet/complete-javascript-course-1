9e9e950f604f646f290e067b877d0395717539fb
Eddie Crochet <eddiecrochet1994@gmail.com>
2020-12-26 16:04:29 -0600

    added functionality to all buttons to add and remove each modal in 2 ways.. also refactored for DRY principle

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -2,14 +2,23 @@
 
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
-const btn = document.querySelector('.close-modal');
+const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 
-for (let i = 0; i < btnsOpenModal.length; i++) {
+const openModal = function() {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+};
+
+const closeModal = function() {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++) 
     //attaches handler to all the buttons thru the loop
     btnsOpenModal[i].addEventListener
-        ('click', function() {
-            console.log('GUTTON LCIKIED>>>');
-            modal.classList.remove('hidden');
-        });
-}
+        ('click', openModal);
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
