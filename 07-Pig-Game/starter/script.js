cc5808a813b5c9ceebeea8ab19a2d7210bbe1f38
Eddie Crochet <eddiecrochet1994@gmail.com>
2021-01-03 16:53:42 -0600

    switching the player

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,6 +1,8 @@
 'use strict';
 
 // Selecting elements 
+const player0El = document.querySelector('.player--0');
+const player0El = document.querySelector('.player--1');
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 const current0El = document.getElementById('current--0');
@@ -15,7 +17,10 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
+//array of player 1 and player 2s respective scores
+const scores = [0, 0];
 let currentScore = 0;
+let activeplayer = 0;
 
 //Rolling dice functionality
 btnRoll.addEventListener('click', function() {
@@ -30,8 +35,16 @@ btnRoll.addEventListener('click', function() {
     if(dice!==1){
         //add dice to current score
         currentScore += dice;    
-        current0El.textContent = currentScore; // CHANGE LATER
-    } else {
+        //selecting respective player 1 and 2 class names dynamically
+        document.getElementById(`current--${activeplayer}`).textContent = currentScore;
+        //current0El.textContent = currentScore; // CHANGE LATER
+        //above code was not dyamic
+    } else { 
+        document.getElementById(`current--${activeplayer}`).textContent = 0;
         //switch to next player
+        activeplayer = activeplayer === 0 ? 1 : 0;
+        //if activePlayer is 0 switch to 1 and vice versa
+
+        currentScore = 0;
     }
 })
