897106f4bb0ecc82c2a357adaf3a27c1daecca88
Eddie Crochet <eddiecrochet1994@gmail.com>
2021-01-03 17:25:39 -0600

    finished switching active player and toggled the styles that make the backgroiund color visible for the active player

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -2,7 +2,7 @@
 
 // Selecting elements 
 const player0El = document.querySelector('.player--0');
-const player0El = document.querySelector('.player--1');
+const player1El = document.querySelector('.player--1');
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 const current0El = document.getElementById('current--0');
@@ -46,5 +46,8 @@ btnRoll.addEventListener('click', function() {
         //if activePlayer is 0 switch to 1 and vice versa
 
         currentScore = 0;
+
+        player0El.classList.toggle('player--active');
+        player1El.classList.toggle('player--active');
     }
 })
