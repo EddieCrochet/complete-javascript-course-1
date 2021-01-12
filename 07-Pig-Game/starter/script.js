82ff1a593930b1c9d57b09e09b8f6596cc194287
Eddie Crochet <eddiecrochet1994@gmail.com>
2021-01-12 13:43:24 -0600

    wrote an init function to follow DRY rules

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -12,16 +12,23 @@ const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
-// Starting conditions
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
+let scores, currentScore, activePlayer, playing;
 
-//array of player 1 and player 2s respective scores
-let scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+const init = function() {
+    //array of player 1 and player 2s respective scores
+    scores = [0, 0];
+    currentScore = 0;
+    activePlayer = 0;
+    playing = true;
+    document.querySelector(`.player--${activePlayer}`).classList.remove('player--winner');
+        // Starting conditions
+    score0El.textContent = 0;
+    score1El.textContent = 0;
+    current0El.textContent = 0;
+    current1El.textContent = 0;
+    diceEl.classList.add('hidden');
+};
+init();
 
 const switchPlayer = function() {
     document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -79,14 +86,4 @@ btnHold.addEventListener('click', function() {
 });
 
 // New Game functionality
-btnNew.addEventListener('click', function () {
-    document.querySelector(`.player--${activePlayer}`).classList.remove('player--winner');
-    playing = true;
-    switchPlayer();
-    score0El.textContent = 0;
-    score1El.textContent = 0;
-    current0El.textContent = 0;
-    current1El.textContent = 0;
-    diceEl.classList.add('hidden');
-    scores = [0, 0];
-});
+btnNew.addEventListener('click', init);
