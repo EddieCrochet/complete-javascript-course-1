ef3acf4917df3687f74c7f63acc70bd157da64b9
Eddie Crochet <eddiecrochet1994@gmail.com>
2021-02-10 14:24:26 -0600

    optional chaining(?.) with the nullish coalescing operator (??)

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -47,6 +47,19 @@ const restaurant = {
 
 };
 
+if(restaurant.openingHours && restaurant.openingHours.mon)
+console.log(restaurant.openingHours.mon.open);
+
+// WITH OPTIONAL CHAINING
+//makes easier to check for bugs we might not otherwise know about
+console.log(restaurant.openingHours.mon?.open);
+console.log(restaurant.openingHours?.mon?.open);
+
+for( const day of weekdays) {
+  const open = restaurant.openingHours[day]?.open ?? 'closed';
+  console.log(`On ${day}, we open at ${open}!`);
+}
+
 /////////////////
 // for-of loop
 /*
