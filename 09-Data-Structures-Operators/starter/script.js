04a290a42441e056ab8c3b3a22a65c2e66a195a7
Eddie Crochet <eddiecrochet1994@gmail.com>
2021-02-10 13:10:26 -0600

    Enhanced Object literals

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -1,18 +1,39 @@
 'use strict';
 
+const weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Thurs', 'Fri', 'Sat', 'Sun'];
+
+const openingHours= {
+  [weekdays[3]]: {
+    open: 12,
+    close: 22,
+  },
+  [weekdays[4]]: {
+    open: 11,
+    close: 23,
+  },
+  [weekdays[5]]: {
+    open: 0, // Open 24 hours
+    close: 24,
+  },
+};
+
 const restaurant = {
   name: 'Classico Italiano',
   location: 'Via Angelo Tavanti 23, Firenze, Italy',
   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+  //before es6, had to reset values to set to object property
+  //openingHours: openingHours,
+  openingHours,
+  //ES6 ENHANCED OBJECT LITERALS WAY above :)
 
-  order: function(starterIndex, mainIndex) {
+  order(starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
 
   //passing in a destructured object to the functio for multiple variables
-  orderDelivery: function( {starterIndex=1, mainIndex=0, time='20:00', address} ) {
+  orderDelivery( {starterIndex=1, mainIndex=0, time='20:00', address} ) {
     console.log(`Order recieved!  ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`);
   },
 
@@ -24,22 +45,11 @@ const restaurant = {
     console.log(mainIngredient, otherIngredients);
   },
 
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0, // Open 24 hours
-      close: 24,
-    },
-  },
 };
 
+/////////////////
+// for-of loop
+/*
 const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
 
 for (const item of menu) console.log(item);
@@ -96,6 +106,7 @@ const game = {
     team2: 6.5,
   },
 };
+*/
 
 ///////////////////////////////////////
 // Coding Challenge #1
