a39cd6e927999c360f1f609d715239a8841d0142
Eddie Crochet <eddiecrochet1994@gmail.com>
2021-05-04 19:28:12 -0500

    finished this coding challenge

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -55,9 +55,12 @@ const restaurant = {
 Let's continue with our football betting app! This time, we have a map with a log of the events that happened during the game. The values are the events themselves, and the keys are the minutes in which each event happened (a football game has 90 minutes plus some extra time).
 
 1. Create an array 'events' of the different game events that happened (no duplicates)
-2. After the game has finished, is was found that the yellow card from minute 64 was unfair. So remove this event from the game events log.
-3. Print the following string to the console: "An event happened, on average, every 9 minutes" (keep in mind that a game has 90 minutes)
-4. Loop over the events and log them to the console, marking whether it's in the first half or second half (after 45 min) of the game, like this:
+2. After the game has finished, is was found that the yellow card from minute 64 was unfair. 
+  So remove this event from the game events log.
+3. Print the following string to the console: "An event happened, on average, every 9 minutes" 
+  (keep in mind that a game has 90 minutes)
+4. Loop over the events and log them to the console, marking whether it's in the first half 
+  or second half (after 45 min) of the game, like this:
       [FIRST HALF] 17: ⚽️ GOAL
 
 GOOD LUCK 😀
@@ -83,14 +86,39 @@ let events = [];
 for (const [key, value] of gameEvents) {
   if(!events.includes(value)) events.push(value)
 }
+/*
+// Can also be done by unpacking a set like...
+const events = [..new Set(gameEvents.values())];
+*/
 console.log(events);
 
+// 2. 
+gameEvents.delete(64);
+console.log(gameEvents);
+
+// 3. 
+const time = [...gameEvents.keys()].pop();
+console.log(`An event happened, on average, every ${time / gameEvents.size} minutes.`);
+
+// 4.
+for(let e of gameEvents){
+  if(e[0] < 45) console.log(`[FIRST HALF] ${e}`)
+  else console.log(`[SECOND HALF] ${e}`)
+}
+
+/*
+// Can also be done...
+for(const [min, event] of gameEvents) {
+  const half = min <= 45 ? 'FIRST' : 'SECOND';
+  console.log(`[${half} HALF] ${min}: ${event}`);
+}
+*/
 
 //////////
 // MAPS ITERATION
 /*
 
-// best way to create maps fro scratch - set method is good for
+// best way to create maps from scratch - set method is good for
 //progrmmatically adding more over time
 const question = new Map([
   ['question', 'What is the best programming language in the world?'],
